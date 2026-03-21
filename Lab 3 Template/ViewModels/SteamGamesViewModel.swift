76c2199044b19e4e030e2aca735160da93596424
E-Camacho314 <115666034+E-Camacho314@users.noreply.github.com>
2026-03-21 13:22:17 -0700

    Calls storeAPI only when clicking on a game

---
+++ Lab 3 Template/ViewModels/SteamGamesViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  SteamGamesViewModel.swift
+//  Lab 3 Template
+//
+//  Created by Erik Camacho on 3/21/26.
+//
+import Foundation
+
+@MainActor
+class SteamGamesViewModel: ObservableObject {
+    
+    @Published var games: [SteamGame] = []
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    func loadGames() async {
+        
+        // prevent re-fetching
+        if !AppManager.steamGames.isEmpty {
+            games = AppManager.steamGames
+            return
+        }
+        
+        isLoading = true
+        
+        do {
+            let fetched = try await SteamService.shared.fetchAllGames()
+            
+            AppManager.steamGames = fetched
+            games = fetched
+            
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+}
